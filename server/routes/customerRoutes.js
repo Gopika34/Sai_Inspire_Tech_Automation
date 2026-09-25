53575aceaa52fac899a8755c71ba19e5f0616d30
Gopiks-s-34 <sivagopika34@gmail.com>
2026-09-25 20:11:38 +0530

    feat: implement customer routes

---
+++ server/routes/customerRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createCustomer,getCustomer,getCustomers,updateCustomer,deleteCustomer } from "../controllers/customerController";
+
+const router= express.Router();
+
+router.post('/',createCustomer);
+router.get('/',getCustomers);
+router.get('/:id',getCustomer);
+router.patch('/:id',updateCustomer);
+router.delete('/:id',deleteCustomer);
+
+export default router;
