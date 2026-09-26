78816696e4fcc7b1ba73b3b2e4be0e852cf9f91d
Gopiks-s-34 <sivagopika34@gmail.com>
2026-09-26 12:20:04 +0530

    Feat(validators): Add validators for handling responses

--- server/routes/leadRoutes.js
+++ server/routes/leadRoutes.js
@@ -11,8 +11,8 @@ import {leadSchema} from "../middleware/validate.js";
 
 const router= express.Router();
 
-router.get("/",validate(leadSchema),getLeads);
-router.post("/",createLead);
+router.get("/",getLeads);
+router.post("/",validate(leadSchema),createLead);
 router.get("/:id",getLead);
 router.patch("/:id",validate(leadSchema.partial()),updateLead);
 router.delete("/:id",deleteLead);
